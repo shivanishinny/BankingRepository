be0696c7d79457797b58d0b4695b93019f048c9e
Rahuul Rathod <shivani24121999@gmail.com>
2021-09-17 17:30:35 +0530

    first commit

---
+++ BankingLibrary/CalcInterest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingLibrary
+{
+    public class CalcInterest : ICalcInterest
+    {
+        public int Calculate(int principle, int RateOfInt, int years)
+        {
+            int SimpleInt = (principle * years * RateOfInt) / 100;
+            return SimpleInt;
+        }
+    }
+}
---
+++ BankingLibrary/ICalcInterest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BankingLibrary
+{
+    public interface ICalcInterest
+    {
+        public int Calculate(int principle, int RateOfInt, int years);
+    }
+}
